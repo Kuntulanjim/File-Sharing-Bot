e401f6aef7d8406bc3e67281bfb2482d8519df9a
Kuntulanjim <87939954+Kuntulanjim@users.noreply.github.com>
2021-07-25 15:01:06 +0000

    UPLOADFILELINK

--- plugins/start.py
+++ plugins/start.py
@@ -94,9 +94,13 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>Kamu Harus Bergabung / Subscribe Channel Telegram Saya
+
+
+
+Klik Coba Lagi Kalau Sudah Bergabung Channel\n\</b>"
     message_text = message.text
-    try:
+    COBA LAGI:
         command, argument = message_text.split()
         text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
